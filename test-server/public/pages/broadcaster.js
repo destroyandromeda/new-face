edfe53695c83c323d7638f8c7fda9ed3aa399ccf
rshayakhmetov <wired1998>
2021-02-19 18:21:24 +0300

    ref

--- test-server/public/pages/broadcaster.js
+++ test-server/public/pages/broadcaster.js
@@ -8,24 +8,40 @@ Promise.all([
 ]).then(startVideo)
 
 function startVideo() {
-    navigator.mediaDevices.getUserMedia(
+    navigator.getUserMedia(
         {video: {}},
         stream => video.srcObject = stream,
         err => console.error(err)
     )
- }
+}
+
+const detection = (canvas, displaySize) => {
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        var ctx = canvas.getContext('2d');
+
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        draw(ctx,resizedDetections)
+
+        // faceapi.draw.drawDetections(canvas, resizedDetections)
+        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+
+
+    }, 100)
+}
+
+function draw(ctx,resizedDetections) {
+    if(resizedDetections[0] && resizedDetections[0].detection){
+        let box = resizedDetections[0].detection.box
+        ctx.strokeRect(box.x, box.y, box.width, box.height);
+    }
+}
 
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas)
-    const displaySize = { width: video.width, height: video.height }
+    const displaySize = {width: video.width, height: video.height}
     faceapi.matchDimensions(canvas, displaySize)
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    }, 100)
+    detection(canvas, displaySize)
 })
